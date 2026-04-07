130985b96200667875b420feccf2d05f285b6d6b
Cru5h3 <lukxi1.ld@gmail.com>
2026-04-07 10:56:43 +0200

    fix: resize input to 256x256 in demo.py and add train.py

--- demo.py
+++ demo.py
@@ -1,16 +1,13 @@
 # -*- coding: utf-8 -*-
-
 import argparse, os, cv2, glob
 os.environ["CUDA_VISIBLE_DEVICES"] = ""
-
 from network.network_pro import Inpaint
 from tqdm import tqdm
 from utils import *
 import warnings
 warnings.filterwarnings('ignore')
-
-# CHANGED: torch explizit importiert, damit der Code nicht nur indirekt über utils funktioniert.
 import torch
+import torch.nn.functional as F
 import numpy as np
 
 parser = argparse.ArgumentParser(
@@ -18,15 +15,11 @@ parser = argparse.ArgumentParser(
     usage='use "%(prog)s --help" for more information',
     formatter_class=argparse.RawTextHelpFormatter
 )
-
 parser.add_argument('--ckpt', required=True, help='Path for the pretrained model')
-
-# CHANGED: Diese Pfade bleiben flexibel. Für ARCADE kannst du hier auch deine vorbereiteten Image-/Mask-Ordner angeben.
 parser.add_argument('--img_path', default="./samples/test_img", help='Path for directory of images. File names must match mask names.')
 parser.add_argument('--mask_path', default="./samples/test_mask", help='Path for directory of masks.')
 parser.add_argument('--output_path', default="./samples/results", help='Path for saving inpainted images')
 parser.add_argument('--device', type=str, default='cpu', choices=['cpu', 'cuda'])
-
 args = parser.parse_args()
 
 assert os.path.exists(args.img_path), "Please check image path"
@@ -35,10 +28,8 @@ assert os.path.exists(args.mask_path), "Please check mask path"
 if not os.path.exists(args.output_path):
     os.mkdir(args.output_path)
 
-# Hyper parameters
 device = torch.device(args.device)
 
-# Pro
 proposed = Inpaint()
 proposed = load_checkpoint(args.ckpt, proposed, device)
 proposed.eval().to(device)
@@ -51,37 +42,33 @@ avg = 0.0
 for step, mask_fn in enumerate(prog_bar):
     fn = os.path.basename(mask_fn)
 
-    # CHANGED: Graustufen statt RGB laden, weil ARCADE 1-Kanal-Bilder hat.
     gt_gray = cv2.imread(os.path.join(args.img_path, fn), cv2.IMREAD_GRAYSCALE)
     assert gt_gray is not None, "Could not read image: {}".format(os.path.join(args.img_path, fn))
 
-    # CHANGED: Auf [-1, 1] normalisieren, aber nur 1 Kanal.
     gt_ = (gt_gray.astype(np.float32) / 255.0) * 2.0 - 1.0
 
-    # CHANGED: Maske als Graustufen laden.
     mask_gray = cv2.imread(mask_fn, cv2.IMREAD_GRAYSCALE)
-    
     assert mask_gray is not None, "Could not read mask: {}".format(mask_fn)
-    
     mask = mask_gray.astype(np.float32) / 255.0
 
-    # CHANGED: Tensor-Shape von (B,3,H,W) auf (B,1,H,W) umgestellt.
-    gt = torch.Tensor(gt_)[None, None, :, :].to(device, dtype=torch.float32)
+    gt   = torch.Tensor(gt_)[None, None, :, :].to(device, dtype=torch.float32)
     mask = torch.Tensor(mask)[None, None, :, :].to(device, dtype=torch.float32)
+
+    # Resize to 256x256 if needed
+    if gt.shape[-1] != 256 or gt.shape[-2] != 256:
+        gt   = F.interpolate(gt,   size=(256, 256), mode='bilinear', align_corners=False)
+        mask = F.interpolate(mask, size=(256, 256), mode='nearest')
 
     with torch.no_grad():
         out_pro = proposed(gt, mask)
 
-    # CHANGED: Ausgabe ist jetzt 1 Kanal, daher kein permute auf 3 Kanäle mehr.
     out_pro = torch.clip(out_pro, -1.0, 1.0) * 0.5 + 0.5
     out_pro = out_pro[0, 0].cpu().detach().numpy() * 255.0
 
-    # CHANGED: PSNR auf 2D-Grauwertbild berechnen.
-    score = psnr(out_pro, (gt_ * 0.5 + 0.5) * 255.0)
+    gt_255 = (gt[0, 0].cpu().numpy() * 0.5 + 0.5) * 255.0
+    score = psnr(out_pro, gt_255)
 
     save_path_ = os.path.join(args.output_path, '{}').format(fn)
-
-    # CHANGED: Als Graustufenbild speichern.
     cv2.imwrite(save_path_, np.clip(out_pro, 0, 255).astype(np.uint8))
 
     avg += score
